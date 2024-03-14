b7c511eea947ba7cabba311bd7a727979a9a39df
Davide <rinna.davide@gmail.com>
2024-03-14 18:29:29 +0100

    created base structure for the Header component. We tested a potential colour palette.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,22 @@
 <template>
-    <div>
-
-    </div>
+    <header class="bg-[#213546] text-[#f1f1f1] border-b-8 border-[#42b983]">
+        <nav class="flex justify-between px-16 py-5 ">
+            <!-- LOGO BLOG -->
+            <div>
+                <p class="text-[30px] font-extrabold">&lt;B-Log()&gt;</p>
+            </div>
+            <!-- navigazione -->
+            <div class="flex text-[20px] gap-8 font-semibold">
+                <button>Articoli</button>
+                <button>Guide</button>
+                <button>Zona Marcello</button>
+            </div>
+            <!-- logo aziendale -->
+            <div>
+                <p class="text-[30px] font-extrabold">&lt;Adams CG/&gt;</p>
+            </div>
+        </nav>
+    </header>
 </template>
 
 <script>
