d4f81954c151b1ed23c51df15d746db4db989f78
Davide <rinna.davide@gmail.com>
2024-03-15 10:19:43 +0100

    tweak header nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,13 @@
 <template>
     <header class="bg-[#213546] text-[#f1f1f1] border-b-8 border-[#42b983]">
-        <nav class="flex justify-between px-16 py-5 ">
+        <nav class="flex justify-between items-center px-16 py-5 ">
             <!-- LOGO BLOG -->
             <div>
-                <p class="text-[30px] font-extrabold">&lt;B.Log()&gt;</p>
+                <p class="text-[30px] font-extrabold">&lt;B.log()&gt;</p>
             </div>
             <!-- navigazione -->
             <div class="flex text-[20px] gap-8 font-semibold">
+                <button>Home</button>
                 <button>Articoli</button>
                 <button>Guide</button>
                 <button>Zona Marcello</button>
