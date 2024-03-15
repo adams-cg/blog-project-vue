923252a02b6953d0d08a6611ded369290aee864d
DavideGenovese <davide.genovese1395@gmail.com>
2024-03-15 12:21:50 +0100

    Carosello

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,50 @@
-<template></template>
-
-<script></script>
+<template>
+    <Carousel v-bind="settings" :breakpoints="breakpoints">
+      <Slide v-for="slide in 10" :key="slide">
+        <div class="">{{ slide }}</div>
+      </Slide>
+  
+      <template #addons>
+        <Navigation />
+      </template>
+    </Carousel>
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue'
+  import { Carousel, Navigation, Slide } from 'vue3-carousel'
+  
+  import 'vue3-carousel/dist/carousel.css'
+  
+  export default defineComponent({
+    name: 'Breakpoints',
+    components: {
+      Carousel,
+      Slide,
+      Navigation,
+    },
+    data: () => ({
+      // carousel settings
+      settings: {
+        itemsToShow: 1,
+        snapAlign: 'center',
+      },
+      // breakpoints are mobile first
+      // any settings not specified will fallback to the carousel settings
+      breakpoints: {
+        // 700px and up
+        700: {
+          itemsToShow: 3.5,
+          snapAlign: 'center',
+        },
+        // 1024 and up
+        1024: {
+          itemsToShow: 5,
+          snapAlign: 'start',
+        },
+      },
+      slide:{
+      }
+    }),
+  })
+  </script>
