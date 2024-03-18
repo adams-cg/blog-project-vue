fad1f1c7ad02bfceb4651e0f8603b9d4d15a8f55
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-03-18 12:13:31 +0100

    articolo completo template

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Article from '../views/Article.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/article',
+      name: 'article',
+      component: Article
     }
   ]
 })
