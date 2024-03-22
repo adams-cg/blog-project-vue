269431d688c0e4d645d21241a911aaa5b778b6e3
AntonioLicitra <antolici@outlook.com>
2024-03-22 09:09:38 +0100

    installazione sanity

---
+++ blog/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'B.log()',
+
+  projectId: 'ir44xo6i',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
