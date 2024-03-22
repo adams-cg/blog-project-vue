8cb0e20c16bcd2596b0a5679c3402ebf9c6fe6ac
Davide <rinna.davide@gmail.com>
2024-03-22 09:57:26 +0100

    added resume

--- blog/schemaTypes/post.js
+++ blog/schemaTypes/post.js
@@ -45,6 +45,11 @@ export default defineType({
       type: 'datetime',
     }),
     defineField({
+      name: 'resume',
+      title: 'Resume',
+      type: 'blockContent'
+    }),
+    defineField({
       name: 'body',
       title: 'Body',
       type: 'blockContent',
