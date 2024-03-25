8fa7cbdcf5d416d9553a132a72077873d9e688da
AntonioLicitra <antolici@outlook.com>
2024-03-25 11:35:46 +0100

    post in evidenza

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,10 @@
 
   <!-- TODO: sezione articolo in evidenza -->
 
+  <p class=" text-[200px]"> {{ articolo.title }}</p>
+
+
+
   <Carousel v-bind="settings" :wrap-around="true" :breakpoints="breakpoints">
     <Slide v-for="(image, index) in images" :key="index">
       <div class="flex justify-center items-center w-[200px] h-[200px] border-2 rounded-lg mb-[100px]">
@@ -71,7 +75,23 @@ export default defineComponent({
       "src/assets/logos_vue.png",
       "src/assets/mdi_github.png",
       "src/assets/Vector.png"
-    ]
-  })
-})
+    ],
+    articoli:[],
+    articolo:{}
+  }),
+  mounted() {
+        axios.get("https://ir44xo6i.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%22post%22%5D+%7B%0A++title%2C%0A++author-%3E%7Bname%7D%2C%0A++mainImage%7B%0A++++asset-%3E%7Burl%7D%0A++%7D%2C%0A++body%5B0%5D%7Bchildren%5B0%5D%7Btext%7D%7D%2C%0A++publishedAt%2C%0A++++categories%5B0%5D-%3E%7Btitle%7D%2C%0A++++resume%5B0%5D%7Bchildren%5B0%5D%7Btext%7D%7D%2C%0A++id%0A%7D")
+        .then(response => {
+            
+            this.articoli = response.data.result
+            this.articoli.sort((a,b) => a.id - b.id )
+            this.articolo = this.articoli[this.articoli.length - 1]
+          
+            
+            
+            
+        }).catch(error => {
+            console.error(error)
+        });
+}})
 </script>
