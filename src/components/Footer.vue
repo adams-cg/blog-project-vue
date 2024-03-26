23385d618aa9faec6f8b2ac298f00e2e9c94858c
giuserai99 <giusepperaineri68@gmail.com>
2024-03-26 16:40:42 +0100

    Aggiunto footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<template>
+    <div class="w-full h-[150px] bg-white mt-[50px] flex items-center justify-around p-[24px] shadow-2xl rounded-t-[50px]">
+        <div>
+            <span class="text-[30px] font-extrabold">&lt;/Adams CG&gt;</span>
+        </div>
+        <div>
+            <p class="font-medium">© 2024 Adams CG. All rights reserved.</p>
+        </div>
+        <div>
+            <ul class="text-[20px] font-thin">
+                <li><a href="">Articoli</a></li>
+                <li><a href="">Guide</a></li>
+                <li><a href="">AboutUs</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
